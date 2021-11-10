76c7efd554bed6919dac111de18aab0c3b57c4a6
Janton2021 <82608002+Janton2021@users.noreply.github.com>
2021-11-10 20:52:19 +0100

    Feat/heroku (#1)
    * chore:add morgan in dependencies
    
    * fix:db
    
    * chore:changed heroku_db_url
    
    * chore: add comment about morgan in README
    
    Co-authored-by: Juan Anton <jantonperez1980@gmail.com>

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,11 @@
-require('dotenv').config()
-const slonik = require('slonik')
+require("dotenv").config();
+const slonik = require("slonik");
 
-const db = slonik.createPool(process.env.SLONIK_URL)
-module.exports = db
+const slonikURL =
+	process.env.NODE_ENV === "production"
+		? process.env.HEROKU_DB_URL
+		: process.env.SLONIK_URL;
+
+const db = slonik.createPool(slonikURL);
+
+module.exports = db;
