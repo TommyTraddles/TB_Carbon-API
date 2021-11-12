b3af6a88c311dbf077d18b863c1cf102e8c78e89
TommyTraddles <jandresnova@gmail.com>
2021-11-12 15:02:01 +0100

    chore: register basic Post

--- src/helpers/queries/auth.js
+++ src/helpers/queries/auth.js
@@ -1,7 +1,26 @@
 const { sql } = require('slonik')
 
-const register = db => async () => {
-  console.info('testing query')
+const exists = async (db, {email, username}) => {
+  return await db.maybeOne(sql`
+    SELECT * FROM users WHERE email = ${email} OR username = ${username}
+  `)
+}
+
+const register = async (db, {email, hash, username, token}) => {
+  try {
+    // confirm doesn't exist
+    const result = await exists(db, {email, username})
+    if(result) throw new Error ('Email taken')
+    
+    // create user 
+    return await db.query(sql`
+    INSERT INTO users ( email, username, hash, token_confirm ) 
+    VALUES (${email}, ${username} , ${hash}, ${token});
+    `)
+  } catch (e) {
+    console.info("> error at 'register' query: ", e.message)
+    return false
+  }
 }
 
 module.exports = {
--- src/routes/auth/_register.js
+++ src/routes/auth/_register.js
@@ -1,15 +1,32 @@
+const { register } = require('../../helpers/queries/auth')
 
 module.exports = db => async (req, res, next) => { 
 
-  const { email, password } = req.body
+  const { email, password, username } = req.body
+  
+  // all fields
 
-  console.info(email)
-  console.info(password)
+
+  // create user
+  const hash = password
+  const token = 'hello'
+  const result = await register(db, {email, hash, username, token})
+
+  if (result === false) {
+    return next({
+      statusCode: 400,
+      error: new Error ('Cannot create user')
+    })
+  }
+
+  // send confirmation mail
+
+
 
   res.status(200)
     .json({
       succes: true,
-      data: 'Auth',
+      data: 'created',
     })
 
 }
