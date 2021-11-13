326109e8b00a7b9e4678967b966a150783e92520
TommyTraddles <jandresnova@gmail.com>
2021-11-13 17:22:45 +0100

    chore: updating email templates

--- src/helpers/templates/index.js
+++ src/helpers/templates/index.js
@@ -1,37 +1,28 @@
+const { activationTemplate } = require('./_activation')
+const { forgotTemplate } = require('./_forgot-pass')
+const { confirmationTemplate } = require('./_confirm')
+
+const url = `${process.env.SERVER_URL}:${process.env.SERVER_PORT}`
+
 const activation = ({ to, token }) => ({
-  from: `" 🤟 eCO2" ${process.env.MAIL_USER}`,
+  from: `" 🤟 equipo eCO2" ${process.env.MAIL_USER}`,
   to,
   subject: "Confirma tu correo",
-  html: `
-  <h4> ☝️ Un último paso </h4>
-  <p>
-  Para acceder, haz clic 
-    <a href="${process.env.SERVER_URL}:${process.env.SERVER_PORT}/auth/confirmation/${token}" target:"_blank">
-    ${process.env.SERVER_URL}:${process.env.SERVER_PORT}/auth/confirmation/${token}
-    </a>
-  </p>
-  `,
+  html: activationTemplate(token, url )
 });
 
 const confirmation = ({ to, username }) => ({
-  from: `" 🤟 eCO2" ${process.env.MAIL_USER}`,
+  from: `" 🤟 equipo eCO2" ${process.env.MAIL_USER}`,
   to,
   subject: `Tu cuenta ha sido verificada`,
-  html: `
-    <h2> Gracias por registrarte ${username} </h2>
-    <p> ${username}, esto es un template de plantilla </p>  
-  `,
+  html: confirmationTemplate(username)
 });
 
 const forgotPass = ({ to, token }) => ({
-  from: `" 🤟 eCO2" ${process.env.MAIL_USER}`,
+  from: `" 🤟 equipo eCO2" ${process.env.MAIL_USER}`,
   to,
   subject: 'Contraseña olvidada',
-  html: `
-  <h2>¿Has olvidado tu contraseña?</h2>
-  <p>Haz clicl <a href="${process.env.SERVER_URL}:${process.env.SERVER_PORT}/auth/password/request?token=${token}&email=${to}"> aquí </a> para poder obtener una nueva contraseña</p>
-  <p>Gracias por tu confianza</p>
-  `,
+  html: forgotTemplate(to, token, url)
 });
 
 module.exports = {
