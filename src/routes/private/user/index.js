38acdcc0b303749dd0bf41de666ee149db40ec05
TommyTraddles <jandresnova@gmail.com>
2021-11-13 12:42:21 +0100

    chore: logout solved

--- src/routes/private/user/index.js
+++ src/routes/private/user/index.js
@@ -2,8 +2,7 @@ const router = require('express').Router()
 
 module.exports = db => {
 
-  router.use('/', require('./app')(db))
-  router.use('/user', require('./user')(db))
+  router.get('/logout', require('./_logout')(db))
 
   return router
 }
