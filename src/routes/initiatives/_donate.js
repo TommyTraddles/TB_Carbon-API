3f05e72b1ab0266adb63f36c2efc3291cdd5e456
TommyTraddles <jandresnova@gmail.com>
2021-11-22 17:40:05 +0100

    chore: backend return payment comprobation

--- src/routes/initiatives/_donate.js
+++ src/routes/initiatives/_donate.js
@@ -1,26 +1,37 @@
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const Stripe = require("stripe");
 
 module.exports = (db) => async (req, res, next) => {
-
   // all data
   const { user } = res.locals;
-  const { id, amount } = req.body;
-  
-  // stripe
-  // const session = await stripe.checkout.sessions.create({
-  //   line_items: [
-  //     {
-  //       price: amount,
-  //     },
-  //   ],
-  //   mode: "payment",
-  //   success_url: `${process.env.CLIENT_URL}/success`,
-  //   cancel_url: `${process.env.CLIENT_URL}/cancel`,
-  // });
+  // ❌ retrieve email by user.locals
+  const email = "ill.better.be.off@gmail.com";
+  const { id, amount, payment } = req.body;
 
-  // respomse
-  res.status(200).json({
-    success: true,
-    data: "thanks for donating",
-  });
+  // ❌ retrieve intiative by ID
+
+  // stripe
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+  try {
+    const pay = await stripe.paymentIntents.create({
+      amount: Number(amount) * 100,
+      currency: "EUR",
+      description: id,
+      payment_method: payment,
+      receipt_email: email,
+      confirm: true,
+    });
+
+    // ❌ SAVE ON DB
+
+    return res.status(200).json({
+      success: true,
+      data: "successfull payment",
+    });
+  } catch (err) {
+    return next({
+      success: false,
+      error: err,
+    });
+  }
 };
